02be6ea54c57f7433398ba543ccfbc2e79b56592
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-09-01 10:28:56 +0300

    feat: add footer, maim adaptive, burger

--- src/index.js
+++ src/index.js
@@ -1,26 +1,38 @@
-//бургер
-const burgerIcon = document.querySelector(".header__burger");
+function createBurger() {
+
+const burgerIcon = document.querySelector(".burger");
 const burgerMenu = document.querySelector(".header__navigation");
 const span = document.querySelector(".burger__icon");
-const opacity = document.querySelector(".opacity");
 
 
 burgerIcon.addEventListener("click", () => {
 	burgerIcon.classList.toggle('active');
 	burgerMenu.classList.toggle('active');
 	span.classList.toggle('active');
-	opacity.classList.toggle('active');
+	document.body.classList.toggle('_lock');
 })
 
-document.addEventListener('click', event => {
-if(event.target.className !== 'burger__icon active' && 
-event.target.className !== 'span active' && 
-event.target.className !== 'menu__body active') {
-	burgerIcon.classList.remove('active');
-	burgerMenu.classList.remove('active');
-	span.classList.remove('active');
-	opacity.classList.remove('active');
+  opacity.addEventListener('click', () => {
+    burgerMenu.classList.remove('active');
+		burgerIcon.classList.remove('active');
+		span.classList.remove('active');
+		document.body.classList.remove('_lock');
+
+  });
+
+  burgerMenu.addEventListener('click', (e) => {
+    const target = e.target;
+		console.log(target)
+    if (target) {
+			burgerMenu.classList.remove('active');
+			burgerIcon.classList.remove('active');
+			span.classList.remove('active');
+			document.body.classList.remove('_lock');
+    }
+  });
+
+
+
+
 }
-})
-
-
+createBurger()
