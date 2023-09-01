3dc856aa8fbae4123f19feb7b9617c5791290684
Alexandra Barmin <barmin.alexandra@gmail.com>
2023-09-01 10:58:27 +0300

    style: style all files

--- src/index.js
+++ src/index.js
@@ -1,48 +1,38 @@
 function createBurger() {
+	const burgerIcon = document.querySelector(".burger");
+	const burgerMenu = document.querySelector(".header__navigation");
+	const span = document.querySelector(".burger__icon");
 
-const burgerIcon = document.querySelector(".burger");
-const burgerMenu = document.querySelector(".header__navigation");
-const span = document.querySelector(".burger__icon");
+	burgerIcon.addEventListener("click", () => {
+		burgerIcon.classList.toggle('active');
+		burgerMenu.classList.toggle('active');
+		span.classList.toggle('active');
+		document.body.classList.toggle('_lock');
+	})
 
-
-burgerIcon.addEventListener("click", () => {
-	burgerIcon.classList.toggle('active');
-	burgerMenu.classList.toggle('active');
-	span.classList.toggle('active');
-	document.body.classList.toggle('_lock');
-})
-
-  burgerMenu.addEventListener('click', () => {
-
-			burgerMenu.classList.remove('active');
-			burgerIcon.classList.remove('active');
-			span.classList.remove('active');
-			document.body.classList.remove('_lock');
-    })
-	}
-
+	burgerMenu.addEventListener('click', () => {
+		burgerMenu.classList.remove('active');
+		burgerIcon.classList.remove('active');
+		span.classList.remove('active');
+		document.body.classList.remove('_lock');
+	})
+}
 createBurger()
 
 function addLanguageChange() {
-
 	const languageBtn = document.querySelector('.language-btn')
 	const languageBtnNoText = document.querySelector('.language-btn__image_globe')
 	const language = document.querySelector('.language-btn__text')
 
-	languageBtn.addEventListener('click', ()=>{
+	languageBtn.addEventListener('click', () => {
 		language.classList.toggle('russian');
-		if(language.classList.contains('russian')){
-		language.textContent ='RU'
-		}else{
-			language.textContent ='En'
-		}
+		language.classList.contains('russian') ? language.textContent = 'RU' : language.textContent = 'En';
 	}
 	)
-	
-	languageBtnNoText.addEventListener('click', ()=>{
+
+	languageBtnNoText.addEventListener('click', () => {
 		languageBtnNoText.classList.toggle('russian');
 	}
 	)
-	
-	}
-	addLanguageChange()
+}
+addLanguageChange()
